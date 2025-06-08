c27c495591dd4085461b7290e6a80ce8ee5f9b29
budi641 <aameendev@gmail.com>
2025-06-08 08:52:19 +0300

    Create projects.js

---
+++ src/data/projects.js
@@ -0,0 +1,16 @@
+{
+  id: "Project Ultra",
+  title: "Project Ultra",
+  shortDescription: "A VR training simulation for an industrial facility.",
+  fullDescription: "A VR training simulation developed for an industrial facility to train new employees on safety protocols and equipment operation in a realistic and immersive environment.",
+  image: projectUltraImage,
+  links: [
+    { type: "youtube", url: "https://www.youtube.com/watch?v=avYfwPt-Ftg&t=14s" }
+  ],
+  technologies: [
+    "Unreal Engine",
+    "C++",
+    "VR Development",
+    "Oculus SDK"
+  ]
+}, 
