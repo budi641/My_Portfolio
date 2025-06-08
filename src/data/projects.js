5cb03f61679f78661bacc695e374115a7fcac233
budi641 <aameendev@gmail.com>
2025-06-08 17:46:43 +0300

    Revert "Update projects.js"
    This reverts commit 6c32f0cddb665d54f6618c31a48ac95868f311d8.

--- src/data/projects.js
+++ src/data/projects.js
@@ -5,7 +5,7 @@
   fullDescription: "A VR training simulation developed for an industrial facility to train new employees on safety protocols and equipment operation in a realistic and immersive environment.",
   image: projectUltraImage,
   links: [
-    { type: "youtube", url: "https://www.youtube.com/watch?v=avYfwPt-Ftg&t=14s&pp=ygUfcHJvamVjdCB1bHRyYSBhYmRlbHJhaG1hbiBhbWVlbg%3D%3D" }
+    { type: "youtube", url: "https://www.youtube.com/watch?v=avYfwPt-Ftg&t=14s" }
   ],
   technologies: [
     "Unreal Engine",
