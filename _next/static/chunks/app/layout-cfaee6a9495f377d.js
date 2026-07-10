eb06d6a2225decd6108e161758f0580b97158f6c
Abdelrahman Ameen <85764244+budi641@users.noreply.github.com>
2026-07-10 19:22:21 +0300

    Updates

---
+++ _next/static/chunks/app/layout-cfaee6a9495f377d.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{1036:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,8432,23))},8432:()=>{}},e=>{var s=s=>e(e.s=s);e.O(0,[417,453,568,358],()=>s(1036)),_N_E=e.O()}]);
---
+++ _next/static/chunks/pages/_error-a8081c3a28df64c0.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[731],{4864:(_,n,e)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_error",function(){return e(6369)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[636,593,792],()=>n(4864)),_N_E=_.O()}]);
