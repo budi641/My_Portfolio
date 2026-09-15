2e5f82a8eedf2146bf65e970b60472f37c7a0fb4
Abdelrahman Ameen <85764244+budi641@users.noreply.github.com>
2026-09-15 21:14:51 +0300

    Make the generated sitemap and robots files work with the static export.

--- app/robots.ts
+++ app/robots.ts
@@ -1,6 +1,8 @@
 import type { MetadataRoute } from "next"
 import { content } from "@/lib/content"
 
+export const dynamic = "force-static"
+
 const pageUrl = content.site.url.replace(/\/$/, "")
 
 export default function robots(): MetadataRoute.Robots {
